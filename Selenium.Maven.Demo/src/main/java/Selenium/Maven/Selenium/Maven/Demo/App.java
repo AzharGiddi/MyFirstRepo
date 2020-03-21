d1ce907d4225526d96e1a487e2ac523f167f8808
Azgid <Azgid@DESKTOP-6JEM0JT>
2020-03-21 23:34:10 +0530

    First Commit

---
+++ Selenium.Maven.Demo/src/main/java/Selenium/Maven/Selenium/Maven/Demo/App.java
@@ -0,0 +1,13 @@
+package Selenium.Maven.Selenium.Maven.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Selenium.Maven.Demo/src/test/java/PracticePackage/MyClass1.java
@@ -0,0 +1,29 @@
+package PracticePackage;
+
+import org.testng.annotations.Test;
+
+public class MyClass1 {
+
+	
+	@Test
+	public void add() {
+		System.out.println("Adding");
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("Substraction");
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("Multiplication");
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("Division");
+	}
+	
+	
+}
